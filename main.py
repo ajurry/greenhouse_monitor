af547a974d749cf7762deaf3f40a1b82aa3bca64
Andy Urry <andrew.j.urry@gmail.com>
2021-08-15 11:14:49 -0700

    Latest code

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+try:
+    import RPi.GPIO as GPIO
+except: 
+    import Mock.GPIO as GPIO
+
 import config_loader.json_config_reader as json_config_reader
 import config_loader.monitor_config as monitor_config
-
 
 def main():
     reader = json_config_reader.JsonConfigReader(r"monitor_config.json")
     config = reader.get_config()
-    print(config.water_control_pump_pin)
+    
+    
 
 if __name__=="__main__":
     main()
--- water_controls/water_pump.py
+++ water_controls/water_pump.py
@@ -1,4 +1,15 @@
+import config_loader.monitor_config as monitor_config
+
+try:
+    import RPi.GPIO as GPIO
+except: 
+    import Mock.GPIO as GPIO
+
 class WaterPump:
     """Allows control of the water pump
     and abstracts Pi related details"""
-    
+    __GPIO_pump_pin: int
+
+    def __init__(self, config: monitor_config.Config):
+        self.GPIO_pump_pin = config.water_control_pump_pin
+        GPIO
